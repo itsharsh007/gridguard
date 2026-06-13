455c26118f2358868ee78aad6051e6c0203653ad
Aishwarya <aishwarya100406@example.com>
2026-06-14 02:42:19 +0530

    fix: make focal_loss import conditional on torch

--- gridguard/imbalance/__init__.py
+++ gridguard/imbalance/__init__.py
@@ -1,4 +1,9 @@
-from .focal_loss import BinaryFocalLoss
 from .sampling import apply_smote, class_balance
 
-__all__ = ["BinaryFocalLoss", "apply_smote", "class_balance"]
+__all__ = ["apply_smote", "class_balance"]
+
+try:
+    from .focal_loss import BinaryFocalLoss
+    __all__ += ["BinaryFocalLoss"]
+except ImportError:
+    pass
